8314a60f075d8198af9cb0284322fa713aeb3974
all-an <all-an@hotmail.com>
2022-02-20 12:36:58 -0300

    erro passando texto no teste

--- README.md
+++ README.md
@@ -35,36 +35,27 @@
 ## Primeira classe de teste:
 
 ```java
-package com.springboot.test;
-
-import org.junit.Test;
-import static io.restassured.RestAssured.given;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class ApiTest {
-	
-	@Test
-	public void getUsers() {
-		// http://localhost:8080/users/
-		String endpoint = "http://localhost:8080/users/";
+@Test
+    public void getUsers() {
+        // http://localhost:8080/users/
+        String endpoint = "http://localhost:8080/users/";
         var response = given().when().get(endpoint).then();
-        response.log().body();
-	}
-	
-	@Test
-    public void getProduct(){
-        String endpoint = "http://localhost:8080/user/";
-        var response =
-                given().
-                    queryParam("id", 4).
-                when().
-                    get(endpoint).
-                then();
         response.log().body();
     }
 
-}
+    @Test
+    public void getUser() {
+        // http://localhost:8080/users/
+        String endpoint = "http://localhost:8080/users/1";
+        var response = given().when().get(endpoint).then();
+        response.log().body();
+    }
+```
+
+## POST :
+
+```java
+// ERRO POST TEXT / JSON SOLUCIONAR
 ```
 
 ## API do Julio de Lima e do Antonio Montanha utilizada para os Testes sob autorização de:
--- springboot-jpa-hibernate/src/main/java/com/springboot/test/ApiTest.java
+++ springboot-jpa-hibernate/src/main/java/com/springboot/test/ApiTest.java
@@ -1,30 +1,55 @@
 package com.springboot.test;
 
+import com.google.gson.Gson;
+import com.springboot.entities.User;
 import org.junit.Test;
 import static io.restassured.RestAssured.given;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ApiTest {
-	
-	@Test
-	public void getUsers() {
-		// http://localhost:8080/users/
-		String endpoint = "http://localhost:8080/users/";
+
+    @Test
+    public void getUsers() {
+        // http://localhost:8080/users/
+        String endpoint = "http://localhost:8080/users/";
         var response = given().when().get(endpoint).then();
         response.log().body();
-	}
-	
-	@Test
-    public void getProduct(){
-        String endpoint = "http://localhost:8080/user/";
-        var response =
-                given().
-                    queryParam("id", 4).
-                when().
-                    get(endpoint).
-                then();
+    }
+
+    @Test
+    public void getUser() {
+        // http://localhost:8080/users/
+        String endpoint = "http://localhost:8080/users/1";
+        var response = given().when().get(endpoint).then();
         response.log().body();
     }
+
+//    @Test
+//    public void getUser(){
+//        String endpoint = "http://localhost:8080/users/";
+//        var response =
+//                given().
+//                        queryParam("id", 1).
+//                        when().
+//                        get(endpoint).
+//                        then();
+//        response.log().body();
+//    }
+//
+//    @Test
+//    public void createUser(){
+//        String endpoint = "http://localhost:8080/users/";
+//        String body = """
+//                {
+//                    "name": "Allan",
+//                    "email": "a@gmail.com",
+//                    "phone": "252342",
+//                    "password": "789789676252342"
+//                }
+//                """;
+//        var response = given().body(body).when().post(endpoint).then();
+//        response.log().body();
+//    }
 
 }
