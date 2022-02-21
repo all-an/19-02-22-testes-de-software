26518e87f0a19746a516378dbab79cc0904544f0
all-an <all-an@hotmail.com>
2022-02-20 22:47:43 -0300

    selenium part 2

---
+++ ex-selenium/Exercise Files/02_04/start/test/SwitchToActiveWindow.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Scanner;
+
+public class SwitchToActiveWindow {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+
+        System.setProperty("webdriver.chrome.driver", "/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        Thread.sleep(20000);
+
+
+        Integer continua = 1;
+        while(continua == 1){
+
+
+            Thread.sleep(5000);
+            WebElement autocomplete = driver.findElement(By.className("_13NKt copyable-text selectable-text"));
+//            autocomplete.sendKeys("Te amo !");
+
+//            WebElement newTabButton = driver.findElement(By.className("_4sWnG"));
+//            newTabButton.click();
+
+        }
+
+        WebElement autocomplete = driver.findElement(By.className("_13NKt copyable-text selectable-text"));
+        autocomplete.sendKeys("Te amo !");
+
+
+
+        Thread.sleep(5000);
+
+        String originalHandle = driver.getWindowHandle();
+
+        for(String handle1: driver.getWindowHandles()) {
+            driver.switchTo().window(handle1);
+        }
+
+        Thread.sleep(3000);
+
+        driver.switchTo().window(originalHandle);
+
+        sc.nextLine();
+        driver.quit();
+    }
+}
--- ex-selenium/Exercise Files/02_05/end/test/SwitchToActiveWindow.java
+++ ex-selenium/Exercise Files/02_05/end/test/SwitchToActiveWindow.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SwitchToActiveWindow {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
@@ -23,6 +24,7 @@ public class SwitchToActiveWindow {
 
         driver.switchTo().window(originalHandle);
 
+        sc.nextLine();
         driver.quit();
     }
 }
